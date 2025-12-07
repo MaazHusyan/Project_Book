9fd83a04f9715ea1f930a744da4132332a14404e
MaazHusyan <mazhusyan120@gmail.com>
2025-12-07 16:49:36 +0500

    T002: Update docusaurus.config.js title to 'Physical and Humanoid Robotics'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Physical & Humanoid Robotics',
+  title: 'Physical and Humanoid Robotics',
   tagline: 'Robots are cool',
   favicon: 'img/favicon.ico',
 
